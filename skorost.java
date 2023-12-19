2ef181453a97180e025bdb57d02eea77b761f0f4
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2023-12-19 21:04:12 +0400

    Update skorost.java

--- skorost.java
+++ skorost.java
@@ -1,23 +1,19 @@
 import java.util.Scanner;
-  public class skorost{
-   public static void main(String[] arg){
-int x=50;
-Scanner s=new Scanner(System.in);
-System.out.println("Tekushaya skorost:");
-int a=s.nextInt();
+public class skorost {
+  public static void main(String[] arg) {
+    int x = 50;
+    Scanner s = new Scanner(System.in);
+    System.out.println("Current speed:");
+    int a = s.nextInt();
 
-if (x>a){
-System.out.println("Mense");
-}
+    if (x > a) {
+      System.out.println("Below normal");
+    } else if (x < a) {
+      System.out.println("Exceeded the speed limit");
+    } else if (x == a) {
+      System.out.println("Set speed");
+    }
 
-else if(x<a){
-System.out.println("Previsel");
-}
-
-else if(x==a){
-System.out.println("Norma");
-}
-
-}
+  }
 
 }
